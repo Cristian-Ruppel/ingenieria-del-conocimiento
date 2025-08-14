9f733292cc1413067e488ebc549bc0919523fd6d
Cristian Ruppel <licenciadocristianruppel@gmail.com>
2025-08-14 18:36:05 -0300

    Feat: Añade Sitemap y robots.txt para SEO profesional

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
   site: 'https://Cristian-Ruppel.github.io',
   base: '/ingenieria-del-conocimiento/',
   // ---
-  integrations: [tailwind()]
+  integrations: [tailwind(), sitemap()]
 });
