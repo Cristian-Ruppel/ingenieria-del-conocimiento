a134157907a1413cd6befade720abdf59d43539b
Cristian Ruppel <licenciadocristianruppel@gmail.com>
2025-08-13 22:32:15 -0300

    Fix: Configura la ruta base para GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
+  site: 'https://Cristian-Ruppel.github.io',
+  base: '/ingenieria-del-conocimiento/',
+  // ---
   integrations: [tailwind()]
 });
